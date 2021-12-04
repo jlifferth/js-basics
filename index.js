d9a6522dee119d85c4fb1dcc4b3b57183fa11c21
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2021-12-04 15:17:03 -0600

    primitive types additional comments

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 let name = 'Juan'; // string literal
 let age = 27; // number literal
 let isApproved = true // boolean literal
-let firstName = undefined;
+let firstName = undefined; // undefined is both a type and a value
 let selectedColor = null;
