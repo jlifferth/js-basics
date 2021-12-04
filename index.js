44f105c84ea847ddec83d907879a93c18f4b9968
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2021-12-04 15:22:38 -0600

    objects

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-// primitive types
-// string, number, boolean, undefined, null
-let name = 'Juan'; // string literal
-let age = 27; // number literal
-let isApproved = true // boolean literal
-let firstName = undefined; // undefined is both a type and a value
-let selectedColor = null;
+// reference types
+
+let person = {
+    name: 'Juan',
+    age: 27
+};
+
+// Dot Notation
+person.name = 'Jonathan';
+
+// Bracket notation
+let selection = 'name';
+person[selection] = 'Mary';
+
+console.log(person.name);
