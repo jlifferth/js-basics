66de4ca91016895c1057729f01e2d2e78d61aea8
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2021-12-04 15:06:12 -0600

    const

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// variables practice
-
-let myName = 'Juan';
-console.log(myName);
-
-// cannot be a reserved keyword
-// should be meaningful
-// cannot start with a number
-// cannot contain a space or hyphen
-// are case sensitive
-
-let firstName = 'Juan';
-let lastName = 'Lafferty';
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
