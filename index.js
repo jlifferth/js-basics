6d3fdd553b9020e3e0395785b52ef310e1f6cd93
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2021-12-04 15:39:13 -0600

    functions, 2

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
+// performing a task
 function greet(name, lastName) {
     console.log('Hello ' + name + ' ' + lastName);
 }
 
-greet('Henry', 'Leeprich');
-greet('Mary', 'Little Lamb');
+// calculating a value
+function square(number) {
+    return number * number;
+}
+
+let number = square(2009347384);
+console.log(number)
