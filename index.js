8ef5a10926cfe494dcf725c52d2cae7024b21493
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2021-12-04 15:27:48 -0600

    objects, cont.

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-// reference types
 
-let person = {
-    name: 'Juan',
-    age: 27
-};
-
-// Dot Notation
-person.name = 'Jonathan';
-
-// Bracket notation
-let selection = 'name';
-person[selection] = 'Mary';
-
-console.log(person.name);
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 1;
+console.log(selectedColors.length);
