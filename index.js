0b318b4148b9abcb1d3edb619687f67f90067f74
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2021-12-04 15:04:38 -0600

    variable practice

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-// This is my first JS code 
-console.log('Hello World');
+// variables practice
 
-let 
+let myName = 'Juan';
+console.log(myName);
+
+// cannot be a reserved keyword
+// should be meaningful
+// cannot start with a number
+// cannot contain a space or hyphen
+// are case sensitive
+
+let firstName = 'Juan';
+let lastName = 'Lafferty';
